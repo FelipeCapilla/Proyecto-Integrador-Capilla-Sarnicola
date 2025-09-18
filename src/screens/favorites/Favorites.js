f1fba287c7e9dfc212705bb3dd29640b0f01b745
sarnicola <fsarnicola@udesa.edu.ar>
2025-09-18 19:13:32 -0300

    wp

--- src/screens/favorites/Favorites.js
+++ src/screens/favorites/Favorites.js
@@ -63,7 +63,7 @@ class Favorites extends Component{
                         />
                         
                     )
-            
+                    
                 }
                 </div>
                 
