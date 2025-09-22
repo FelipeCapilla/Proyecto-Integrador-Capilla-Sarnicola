77e3a56bcb6c2c5b4ae07fdede42e9639de96b57
sarnicola <fsarnicola@udesa.edu.ar>
2025-09-22 08:41:26 -0300

    formgeneral

---
+++ src/components/formularioGeneral/FormularioGeneral.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class FormularioGeneral extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            busqueda: ''
+        }
+    }
+
+    controlarForm(evento){
+        evento.preventDefault()
+        this.props.history.push('/resultados/' + this.state.busqueda)
+    }
+
+    controlarInput(evento){
+        this.setState({
+            busqueda: evento.target.value
+        })
+
+    }
+
+  render() {
+    return (
+        <form onSubmit={(evento) => this.controlarForm(evento)}>
+        <input onChange={(evento) => this.controlarInput(evento)}/>
+        <button>Buscar</button>
+        </form>
+    )
+  }
+}
+
+export default  FormularioGeneral
