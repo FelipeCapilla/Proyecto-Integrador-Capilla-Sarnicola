d74d318ce8bb546b4144d3ee2e443b0b1f9c772a
Felipe Capilla <fcapilla@udesa.edu.ar>
2025-09-22 08:58:24 -0300

    favorites

--- src/screens/notfound/NotFound.js
+++ src/screens/notfound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div>
-      <img src='Hombre-elegante-entienda-de-joyas.png' alt='' />
+      <img src='/img/Hombre-elegante-entienda-de-joyas.png' alt='' />
     </div>
   )
 }
